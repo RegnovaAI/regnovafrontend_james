ca71e45294f02d9df11c3d28c11d2b3dce88d691
founder@regnovaAI.com <founder@regnovaAI.com>
2025-05-06 06:35:04 +0200

    View update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  // theme: {
+  //   extend: {},
+  // },
+  // plugins: [],
+ // Added on 6th may change look.
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3B82F6', // tailwind blue
+        accent: '#9333EA',  // violet
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  
+
+
+
 };
